403cd8c655365ad2bb1666dc13fa753f03a5bb09
olakunle makanjuola <makanjuolakunle@gmail.com>
2018-03-25 05:55:52 +0100

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const {ObjectID} = require('mongodb');
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
@@ -26,6 +27,23 @@ app.get('/todos', (req, res) => {
         res.send({todos});
     }, (e) => {
         res.status(400).send(e);
+    });
+});
+
+app.get('/todos/:id', (req, res) => {
+    let id = req.params.id;
+
+    if(!ObjectID.isValid(id)) {
+        res.status(404).send('Oopss! The address you request cannot be found.');
+    }
+    
+    Todo.findById(id).then((todo) => {
+        if(!todo) {
+            res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
+            res.status(400).send();
     });
 });
 
